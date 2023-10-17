cceb093b1ebc2a1ac0d086b353e94fc4cb142654
LeonardoA77 <Lemniscate4356@Knights.ucf.edu>
2023-10-17 16:44:31 +0000

    yep

--- theme/book.js
+++ theme/book.js
@@ -251,6 +251,17 @@ function playground_text(playground, hidden = true) {
           pre_block.insertBefore(buttons, pre_block.firstChild);
         }
 
+        var runCodeButton = document.createElement("button");
+        runCodeButton.className = "fa fa-play play-button";
+        runCodeButton.hidden = true;
+        runCodeButton.title = "Run this code";
+        runCodeButton.setAttribute("aria-label", runCodeButton.title);
+
+        buttons.insertBefore(runCodeButton, buttons.firstChild);
+        runCodeButton.addEventListener("click", function (e) {
+        run_rust_code(pre_block);
+        });
+        
         var clipButton = document.createElement("button");
         clipButton.className = "fa fa-copy clip-button";
         clipButton.title = "Copy to clipboard";
